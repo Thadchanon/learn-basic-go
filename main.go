f71e73eb7ebe262054a16dac2486e46c6e8d5f93
Thadchanon <thadchanonjirarat@gmail.com>
2023-12-11 02:50:35 +0700

    mini workshop: how to variable in go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	title := "Avengers: Endgame"
+	year := 2019
+	rating := 8.4
+	genre := "Sci-Fi"
+	isSuperhero := true
+
+	fmt.Println("เรื่อง:", title)
+	fmt.Println("ปี:", year)
+	fmt.Println("เรตติ้ง:", rating)
+	fmt.Println("ประเภท:", genre)
+	fmt.Println("ซุปเปอร์ฮีโร่:", isSuperhero)
+
+	// **raw string**
+
+	/* 	movie := `เรื่อง: Avengers: Endgame
+	ปี: 2019
+	เรตติ้ง: 8.4
+	ประเภท: Sci-Fi
+	ซุปเปอร์ฮีโร่: true`
+	fmt.Println(movie) */
+}
