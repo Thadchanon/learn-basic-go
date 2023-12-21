cf7e9eecd3d7f589d9fa9c91b3c96ebd5f246b28
Thadchanon <thadchanonjirarat@gmail.com>
2023-12-22 03:12:00 +0700

    feat: add Review func, FindMovieName func, BuyTicket func

--- main.go
+++ main.go
@@ -2,130 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
-	// workshops "github.com/Thadchanon/learn-basic-go/workshops" // for workshops
+
+	"github.com/Thadchanon/learn-basic-go/movie"
+
+	"github.com/Thadchanon/learn-basic-go/ticket"
 )
 
+func init() {
+	fmt.Println("init: main")
+}
+
 func main() {
-	// msg := "Hello Go!"
-	// age := 24
-	// price := 22.522
-	// check := true
-
-	// var rn rune = 'a'
-
-	// fmt.Printf("type: %T -- msg: %#v\n", msg, msg)       // fmt.Printf("msg: %s\n", msg)
-	// fmt.Printf("type: %T -- age: %#v\n", age, age)       // fmt.Printf("age: %d\n", age)
-	// fmt.Printf("type: %T -- price: %#v\n", price, price) // fmt.Printf("price: %.2f\n", price)
-	// fmt.Printf("type: %T -- check: %#v\n", check, check) // fmt.Printf("check: %t\n", check)
-	// fmt.Printf("type: %T -- rune: %#v\n", rn, rn)
-	// fmt.Printf("rune: %c\n", rn) // f = format, %c =  print character, \n = new line
-
-	/* 	num := 34
-
-	   	if num == 34 && (num > 30 || num < 39) {
-	   		fmt.Println("Yes!! it's Thirty four.")
-	   	} else if num == 35 {
-	   		fmt.Println("It's Thirty five.")
-	   	} else {
-	   		fmt.Println("No!! it's not Thirty four.")
-	   	}
-
-	   	if num%2 == 0 {
-	   		fmt.Println("It's even number.")
-	   	} else {
-	   		fmt.Println("It's odd number.")
-	   	}
-
-	   	lim := 225.0
-
-	   	if v := math.Pow(30, 2); v < lim { // Pow uses calculations using exponents, v scope in if
-	   		fmt.Println("x power n is:", v)
-	   	} else {
-	   		fmt.Printf("x power n is %g over limit %g.\n", v, lim)
-	   	}
-
-	   	switch today := "Saturday"; today {
-	   	case "Saturday":
-	   		fmt.Println("Today is Saturday")
-	   	case "Monday", "Tueday":
-	   		fmt.Println("Today is weekdays")
-	   	default:
-	   		fmt.Printf("Today is %v\n", today)
-	   	}  */
-
-	/* 	sum := 0
-
-	   	for i := 0; i < 5; i++ {
-	   		sum += i
-	   		fmt.Println("i:", i, "sum:", sum)
-	   	}
-	   	fmt.Println("sum:", sum)
-
-	   	skills := [3]string{"js", "go", "python"}
-
-	   	for i := 0; i < len(skills); i++ {
-	   		fmt.Println(skills[i])
-	   	}
-
-	   	for i := range skills {
-	   		fmt.Println(skills[i])
-	   	} */
-
-	skills := []string{"js", "go", "python"}
-	fmt.Printf("length: %d -- val;%#v\n", len(skills), skills)
-
-	skills = append(skills, "ruby")
-	fmt.Printf("length: %d -- val;%#v\n", len(skills), skills)
-
-	// workshops.PrintfWs() // for workshop 1
-	// workshops.PrintlnWs() // for workshop 2
-	// workshops.IfElse() // for workshop 3
-	// workshops.SwitchCase() // for workshop 4
-	// workshops.Function() // for workshop 5
-	// workshops.Array() // for workshop 6
-	// workshops.For() // for workshop 7
-	// workshops.Slice() // for workshop 8
-	// workshops.Struct() // for workshop 9
-	// workshops.Method() // for workshop 10
-	// workshops.Pointer() // for workshop 11
-	// workshops.Interface() // for workshop 12
-	// workshops.Maps() // for workshop 13
-}
-
-var add func(float64, float64) float64 = func(x, y float64) float64 {
-	return x + y
-}
-
-/* func add(x, y float64) float64 {
-	return x + y
-} */
-
-func split(sum int) (x int, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
-
-func swap(x, y string) (string, string) {
-	return y, x
-}
-
-func compute(fn func(float64, float64) float64) float64 {
-	v := fn(42, 13)
-	return v
-}
-
-func hypot(x, y float64) float64 {
-	return math.Sqrt(x*y + y*x)
-}
-
-func adder() (func() int, func() int) {
-	sum := 0
-	return func() int {
-			sum = sum + 1
-			return sum
-		}, func() int {
-			return sum
-		}
+	movieName := movie.FindMovieName("tt4154796")
+	fmt.Println(movieName)
+	movie.Review(movieName, 8.4)
+	ticket.BuyTicket(movieName)
 }
